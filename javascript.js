5cadffc62252732e9e4be4cdf9c539a8853c1a3e
Pedro Alburqueque <prolsu88@gmail.com>
2020-09-29 03:54:41 -0400

    added buttons to change text size for user

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/* detail 1 */
+
+document.getElementById("default-text-size").addEventListener("click", function myFunction() {
+document.getElementById("benefit-details").style.fontSize = "100%";
+});
+
+document.getElementById("medium-text-size").addEventListener("click", function myFunction() {
+document.getElementById("benefit-details").style.fontSize = "125%";
+});
+
+document.getElementById("large-text-size").addEventListener("click", function myFunction() {
+document.getElementById("benefit-details").style.fontSize = "150%";
+});
