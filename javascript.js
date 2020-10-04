34dd36e95b0a6fdf6a5fc85bb0ad98ede9bac7c7
Pedro Alburqueque <prolsu88@gmail.com>
2020-10-03 23:10:39 -0400

    added last changes/homework ready to submit

--- javascript.js
+++ javascript.js
@@ -1,27 +1,27 @@
 /* benefit headers and details */
 
 document.getElementById("small-text-size").addEventListener("click", function myFunction() {
-document.getElementById("benefit-details").style.fontSize = "110%";
+document.getElementById("benefit-details").style.fontSize = "115%";
 });
 
 document.getElementById("medium-text-size").addEventListener("click", function myFunction() {
-document.getElementById("benefit-details").style.fontSize = "120%";
+document.getElementById("benefit-details").style.fontSize = "125%";
 });
 
 document.getElementById("large-text-size").addEventListener("click", function myFunction() {
-document.getElementById("benefit-details").style.fontSize = "130%";
+document.getElementById("benefit-details").style.fontSize = "135%";
 });
 
 /* link headers and details */
 
 document.getElementById("small-text-size").addEventListener("click", function myFunction() {
-document.getElementById("contents").style.fontSize = "110%";
+document.getElementById("contents").style.fontSize = "115%";
 });
     
 document.getElementById("medium-text-size").addEventListener("click", function myFunction() {
-document.getElementById("contents").style.fontSize = "120%";
+document.getElementById("contents").style.fontSize = "125%";
 });
     
 document.getElementById("large-text-size").addEventListener("click", function myFunction() {
-document.getElementById("contents").style.fontSize = "130%";
+document.getElementById("contents").style.fontSize = "135%";
 });
